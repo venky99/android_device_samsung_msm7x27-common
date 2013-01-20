4465bd6966a7b9e7ae4b9d3a2e8c6767385dd5b4
Giovanni Santini <itachi.sama.amaterasu@gmail.com>
2013-01-20 10:07:02 +0100

    Build libcamera with updated one

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -18,6 +18,6 @@ LOCAL_C_INCLUDES       += hardware/qcom/display/libgralloc
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
 LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
 
-LOCAL_LDFLAGS	       += -L$(LOCAL_PATH) -lcamera
+LOCAL_LDFLAGS	       += -Lvendor/samsung/gio/proprietary/lib -lcamera
 
 include $(BUILD_SHARED_LIBRARY)
