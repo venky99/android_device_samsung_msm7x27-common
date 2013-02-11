6ffa66c2b7a3d7a3f8c8c8d20c02b0344778db1e
Grigori Goronzy <greg@blackbox>
2013-02-11 13:37:07 +0100

    FM Radio support
    This is a combination of 7 commits:
    
    FM Radio support (part 1)
    si4708: configure de-emphasis according to selected band
    FM radio support (part 2)
    Fix FM audio routing
    Build libfmradio by default
    si4708: add RDS support
    si4708: use mute IOCTL
    
    Change-Id: I9e1c04b4262ad525a2c3adccf1aaead684e528fd
    
    Conflicts:
    	BoardConfig.mk
    	device.mk
    	libaudio/Android.mk
    	libaudio/AudioHardware.cpp
    	libaudio/AudioHardware.h
    	libaudio/AudioPolicyManager.cpp
    	prebuilt/root/ueventd.blade.rc

--- libaudio/AudioHardware.h
+++ libaudio/AudioHardware.h
@@ -1,6 +1,5 @@
 /*
 ** Copyright 2008, The Android Open-Source Project
-** Copyright (c) 2012, Code Aurora Forum. All rights reserved.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -27,13 +26,13 @@
 #include <hardware_legacy/AudioHardwareBase.h>
 
 extern "C" {
-#include <linux/msm_audio.h>
-#include <linux/msm_audio_voicememo.h>
+#include "msm_audio.h"
+#include "msm_audio_voicememo.h"
 }
 
+using namespace android;
+
 namespace android_audio_legacy {
-using android::SortedVector;
-using android::Mutex;
 
 // ----------------------------------------------------------------------------
 // Kernel driver interface
@@ -52,13 +51,13 @@ using android::Mutex;
 #define EQ_MAX_BAND_NUM 12
 
 #define ADRC_ENABLE  0x0001
-#define ADRC_DISABLE 0xFFFE
+#define ADRC_DISABLE 0x0000
 #define EQ_ENABLE    0x0002
-#define EQ_DISABLE   0xFFFD
+#define EQ_DISABLE   0x0000
 #define RX_IIR_ENABLE  0x0004
-#define RX_IIR_DISABLE 0xFFFB
+#define RX_IIR_DISABLE 0x0000
 #define MBADRC_ENABLE  0x0010
-#define MBADRC_DISABLE 0xFFEF
+#define MBADRC_DISABLE 0x0000
 
 #define AGC_ENABLE     0x0001
 #define NS_ENABLE      0x0002
@@ -147,13 +146,14 @@ enum tty_modes {
 #define CODEC_TYPE_PCM 0
 #define AUDIO_HW_NUM_OUT_BUF 2  // Number of buffers in audio driver for output
 // TODO: determine actual audio DSP and hardware latency
-#define AUDIO_HW_OUT_LATENCY_MS 40  // Additionnal latency introduced by audio DSP and hardware in ms
+#define AUDIO_HW_OUT_LATENCY_MS 0  // Additionnal latency introduced by audio DSP and hardware in ms
 
 #define AUDIO_HW_IN_SAMPLERATE 8000                 // Default audio input sample rate
 #define AUDIO_HW_IN_CHANNELS (AudioSystem::CHANNEL_IN_MONO) // Default audio input channel mask
 #define AUDIO_HW_IN_BUFFERSIZE 2048                 // Default audio input buffer size
 #define AUDIO_HW_IN_FORMAT (AudioSystem::PCM_16_BIT)  // Default audio input sample format
 // ----------------------------------------------------------------------------
+
 
 class AudioHardware : public  AudioHardwareBase
 {
@@ -167,7 +167,7 @@ public:
 
     virtual status_t    setVoiceVolume(float volume);
     virtual status_t    setMasterVolume(float volume);
-#ifdef HAVE_FM_RADIO
+#ifdef QCOM_FM_ENABLED
     virtual status_t    setFmVolume(float volume);
 #endif
     virtual status_t    setMode(int mode);
@@ -214,8 +214,8 @@ private:
     uint32_t    getInputSampleRate(uint32_t sampleRate);
     bool        checkOutputStandby();
     status_t    doRouting(AudioStreamInMSM72xx *input);
-#ifdef HAVE_FM_RADIO
-    status_t    setFmOnOff(bool onoff);
+#ifdef QCOM_FM_ENABLED
+    status_t    setFmOnOff(int onoff);
 #endif
     AudioStreamInMSM72xx*   getActiveInput_l();
 
@@ -282,8 +282,10 @@ private:
         virtual unsigned int  getInputFramesLost() const { return 0; }
                 uint32_t    devices() { return mDevices; }
                 int         state() const { return mState; }
-        virtual status_t    addAudioEffect(effect_interface_s**) { return 0;}
-        virtual status_t    removeAudioEffect(effect_interface_s**) { return 0;}
+
+        // Stubs (ICS)
+        virtual status_t addAudioEffect(effect_handle_t effect) { return INVALID_OPERATION; }
+        virtual status_t removeAudioEffect(effect_handle_t effect) { return INVALID_OPERATION; }
 
     private:
                 AudioHardware* mHardware;
@@ -305,22 +307,23 @@ private:
             bool        mBluetoothNrec;
             uint32_t    mBluetoothId;
             AudioStreamOutMSM72xx*  mOutput;
-            SortedVector <AudioStreamInMSM72xx*>   mInputs;
+            android::SortedVector <AudioStreamInMSM72xx*>   mInputs;
 
             msm_snd_endpoint *mSndEndpoints;
             int mNumSndEndpoints;
             int mCurSndDevice;
+	    int mFmRadioEnabled;
+	    int mFmPrev;
+	    int mFmVolume;
             int m7xsnddriverfd;
-            bool mDualMicEnabled;
-            int  mTtyMode;
-            bool mBuiltinMicSelected;
-#ifdef HAVE_FM_RADIO
-            int mFmRadioEnabled;
-            int mFmPrev;
-#endif
+            int fmfd;
+            bool        mDualMicEnabled;
+            int         mTtyMode;
+
+            bool        mBuiltinMicSelected;
 
      friend class AudioStreamInMSM72xx;
-            Mutex       mLock;
+            android::Mutex       mLock;
 };
 
 // ----------------------------------------------------------------------------
