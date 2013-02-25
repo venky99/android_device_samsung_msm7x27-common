3e5bdbd8918b580c5cd08d6c9c04d61ff3282019
mackief <davidepucci21@gmail.com>
2013-02-25 14:49:47 +0100

    Revert: Add YouTube init script && cleanings

--- common.mk
+++ common.mk
@@ -118,9 +118,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/samsung/msm7x27-common/prebuilt/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
 
-## init.d
+# GPS conf
 PRODUCT_COPY_FILES += \
-    device/samsung/msm7x27-common/prebuilt/etc/init.d/999youtube:system/etc/init.d/999youtube
+    device/samsung/msm7x27-common/prebuilt/etc/gps.conf:system/etc/gps.conf
+
+## Loop ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.call_ring.delay=3000
 
 ## Other
 PRODUCT_LOCALES += en
