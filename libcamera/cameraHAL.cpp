73c5c93ff0c4d3a71233d1b5c43e37f02cdc9790
erikcas <erikcas1972@gmail.com>
2013-03-23 00:56:37 +0100

    fix camera build

--- libcamera/cameraHAL.cpp
+++ libcamera/cameraHAL.cpp
@@ -56,15 +56,15 @@ static struct hw_module_methods_t camera_module_methods = {
 
 camera_module_t HAL_MODULE_INFO_SYM = {
     common: {
-        tag: HARDWARE_MODULE_TAG,
-        module_api_version: CAMERA_DEVICE_API_VERSION_1_0,
-        hal_api_version: 0,
-        id: CAMERA_HARDWARE_MODULE_ID,
-        name: "Camera HAL",
-        author: "Zhibin Wu & Marcin Chojnacki & Pavel Kirpichyov",
-        methods: &camera_module_methods,
-        dso: NULL, /* remove compilation warnings */
-        reserved: {0}, /* remove compilation warnings */
+            tag: HARDWARE_MODULE_TAG,
+            version_major: 1,
+            version_minor: 0,
+            id: CAMERA_HARDWARE_MODULE_ID,
+            name: "Camera HAL",
+            author: "Zhibin Wu & Marcin Chojnacki & Pavel Kirpichyov",
+            methods: &camera_module_methods,
+            dso: NULL, /* remove compilation warnings */
+            reserved: {0}, /* remove compilation warnings */
     },
     get_number_of_cameras: camera_get_number_of_cameras,
     get_camera_info: camera_get_camera_info,
