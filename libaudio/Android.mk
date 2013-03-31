856a3b8e07c47c1b7131c4948ad6da760156ae66
erikcas <erikcas1972@gmail.com>
2013-03-31 22:16:10 +0200

    Revert "FM Radio support"
    This reverts commit 494144bf85f5424210bd2b67f9c379c4e92d2346.

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -1,37 +1,41 @@
-LOCAL_PATH:= $(call my-dir)
+
+ifneq ($(BUILD_TINY_ANDROID),true)
+ifeq ($(TARGET_PROVIDES_LIBAUDIO), true)
+
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= \
-    AudioPolicyManager.cpp
+LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_STATIC_LIBRARIES := libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy    
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     libmedia
 
-LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
-LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libmedia_helper
-LOCAL_MODULE:= audio_policy.blade
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
+LOCAL_SRC_FILES:= AudioPolicyManager.cpp
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
   LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-    LOCAL_CFLAGS += -DQCOM_FM_ENABLED
+ifeq ($(BOARD_HAVE_FM_RADIO),true)
+#  LOCAL_CFLAGS += -DHAVE_FM_RADIO
 endif
 
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio.primary.blade
+LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw	
+LOCAL_STATIC_LIBRARIES += libmedia_helper	
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libmedia_helper libaudiohw_legacy
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
@@ -40,26 +44,19 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     libdl
 
-ifeq ($TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
-LOCAL_LDLIBS += -ldl
-endif
-
-ifneq ($(TARGET_SIMULATOR),true)
-LOCAL_SHARED_LIBRARIES += libdl
-endif
-
-LOCAL_SRC_FILES += \
-    AudioHardware.cpp \
-    audio_hw_hal.cpp
+LOCAL_SRC_FILES += AudioHardware.cpp
 
 LOCAL_CFLAGS += -fno-short-enums
 
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-    LOCAL_CFLAGS += -DQCOM_FM_ENABLED
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+#  LOCAL_SHARED_LIBRARIES += audio.a2dp.default
 endif
 
-#ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-#  LOCAL_SHARED_LIBRARIES += audio.a2dp.default libbinder
-#endif
+ifeq ($(BOARD_HAVE_FM_RADIO),true)
+#  LOCAL_CFLAGS += -DHAVE_FM_RADIO
+endif
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif # TARGET_PROVIDES_LIBAUDIO
+endif # not BUILD_TINY_ANDROID
