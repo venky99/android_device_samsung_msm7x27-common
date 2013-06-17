4729bbaa36b2d2b96c12011b860a641e17691d32
Erik Castricum <erikcas1972@gmail.com>
2013-06-17 14:25:52 +0200

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,5 +139,4 @@ BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/msm7x27-common/recovery
 TARGET_RECOVERY_INITRC := device/samsung/msm7x27-common/recovery/recovery.rc
 TARGET_RECOVERY_FSTAB := device/samsung/msm7x27-common/recovery/recovery.fstab
 
-WITH_DEXPREOPT := true
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/msm7x27-common/releasetools/ota_from_target_files
