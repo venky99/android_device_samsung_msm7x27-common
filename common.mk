5e6ed56f3a3c40e2301b00ac5b6734a9108e7c0e
erikcas <erikcas1972@gmail.com>
2013-08-17 08:38:00 +0200

    Revert "Revert "rework ramdisk""
    This reverts commit cfb6e0e6545a3926a11c09425a3d7ff09bc84abb.

--- common.mk
+++ common.mk
@@ -151,7 +151,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ## Example: -GT-I5500 becomes gt-i5500board, -GT-S5830 becomes gt-s5830board, and so on.
 SAMSUNG_BOOTLOADER := $(shell echo $(PRODUCT_VERSION_DEVICE_SPECIFIC)board | tr '[A-Z]' '[a-z]' | cut -c 2-)
 PRODUCT_COPY_FILES += \
-    device/samsung/msm7x27-common/ramdisk/init.msm7x27.rc:root/init.$(SAMSUNG_BOOTLOADER).rc \
     device/samsung/msm7x27-common/ramdisk/init.msm7x27.bluez.rc:root/init.$(SAMSUNG_BOOTLOADER).bluez.rc \
     device/samsung/msm7x27-common/ramdisk/init.msm7x27.parts.rc:root/init.$(SAMSUNG_BOOTLOADER).parts.rc \
     device/samsung/msm7x27-common/ramdisk/init.msm7x27.usb.rc:root/init.$(SAMSUNG_BOOTLOADER).usb.rc \
