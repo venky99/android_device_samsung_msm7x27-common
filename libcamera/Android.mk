c75f56ec06c1b7a2bfb73d06dbdbf3599cce6087
mackief <davidepucci21@gmail.com>
2013-08-20 10:12:53 +0200

    Updated libcamera Excluded README and libcamera.so: camera.msm7x27 module imports prebuilt libcamera from vendor/samsung.

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -17,6 +17,6 @@ LOCAL_C_INCLUDES       += hardware/qcom/display/libgralloc
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
 LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
 
-LOCAL_LDFLAGS	       += -L$(LOCAL_PATH) -lcamera
+LOCAL_LDFLAGS         += -Lvendor/samsung/gio/proprietary/lib -lcamera
 
 include $(BUILD_SHARED_LIBRARY)
