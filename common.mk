791b81efb7f6ee6e10d0175908c0b82eb18efd59
erikcas <erikcas1972@gmail.com>
2013-08-21 11:48:10 +0200

    Revert "Testing again: Adjust ramdisk"
    This reverts commit 8a5c2ab53f2e523e129bbe51bb2420b457d93f9a.

--- common.mk
+++ common.mk
@@ -88,10 +88,6 @@ PRODUCT_PACKAGES += \
     brcm_patchram_plus \
     setup_fs
 
-## Vold config
-PRODUCT_COPY_FILES += \
-    device/samsung/msm7x27-common/prebuilt/etc/vold.fstab:system/etc/vold.fstab
-
 ## Hardware properties
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
@@ -152,7 +148,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 SAMSUNG_BOOTLOADER := $(shell echo $(PRODUCT_VERSION_DEVICE_SPECIFIC)board | tr '[A-Z]' '[a-z]' | cut -c 2-)
 
 PRODUCT_PACKAGES += \
-    fstab.$(SAMSUNG_BOOTLOADER) \
+    fstab.msm7x27 \
     charging.rle \
     init.$(SAMSUNG_BOOTLOADER).rc \
     init.$(SAMSUNG_BOOTLOADER).bluez.rc \
